a310e0642bcd3317ae9b30328a393361031cd0c2
JimmyADeLeon <142259586+JimmyADeLeon@users.noreply.github.com>
2024-01-24 16:41:46 -0800

    Homework 19

---
+++ index.js
@@ -0,0 +1,31 @@
+
+console.log("Your index.js file is loaded correctly.");
+$( document ).ready(function() {});
+$( "a)" ).on( "click", function() {});
+$.ajax({
+  url: "someurl.php",
+  success: function() {}
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  <script> src="js/index.js></script>"
+</html>; 
---
+++ js/index.js
@@ -0,0 +1,4 @@
+<div style="width: 171px; height: 93px; position: relative; flex-direction: column; justify-content: flex-start; align-items: flex-start; display: inline-flex">
+  <div style="width: 171px; height: 93px; background: linear-gradient(0deg, #C8C6C6 0%, #C8C6C6 100%), linear-gradient(0deg, #757171 0%, #757171 100%)"></div>
+  <div style="color: white; font-size: 24px; font-family: Inter; font-weight: 400; word-wrap: break-word">Wire Frames</div>
+</div>
